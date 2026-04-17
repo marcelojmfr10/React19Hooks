c3680ce59d2302f5ba94da448a54430cf83be6f1
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-04-16 23:26:56 -0600

    Estilos con tailwind, useState y useEffect

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HooksApp } from "./HooksApp";
+// import { HooksApp } from "./HooksApp";
+// import { TrafficLight } from "./01-useState/TrafficLight";
+// import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
+import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
+
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    {/* <TrafficLight /> */}
+    {/* <TrafficLightWithEffect /> */}
+    <TrafficLightWithHook />
   </StrictMode>,
 );
