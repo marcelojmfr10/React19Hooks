0ed2848b7ec224fb99cba1c5e05b30a729a165e3
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-04-20 20:15:58 -0600

    Custom hook

--- src/02-useEffect/TrafficLightWithHook.tsx
+++ src/02-useEffect/TrafficLightWithHook.tsx
@@ -1,51 +1,8 @@
-import { useEffect, useState } from "react";
 import { useTrafficLight } from "../hooks/useTrafficLight";
 
-const colors = {
-  red: "bg-red-500 animate-pulse",
-  yellow: "bg-yellow-500 animate-pulse",
-  green: "bg-green-500 animate-pulse",
-};
-
-type TrafficLightColor = keyof typeof colors;
-
 export const TrafficLightWithHook = () => {
-  const {} = useTrafficLight();
-
-  const [light, setLight] = useState<TrafficLightColor>("red");
-  const [countdown, setCountdown] = useState(5);
-
-  useEffect(() => {
-    if (countdown === 0) return;
-
-    const intervalId = setInterval(() => {
-      setCountdown((prev) => prev - 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [countdown, light]);
-
-  useEffect(() => {
-    if (countdown > 0) return;
-
-    setCountdown(5);
-    if (light === "red") {
-      setLight("green");
-      return;
-    }
-
-    if (light === "yellow") {
-      setLight("red");
-      return;
-    }
-
-    if (light === "green") {
-      setLight("yellow");
-      return;
-    }
-  }, [countdown, light]);
+  const { countdown, light, percentage, greenLight, redLight, yellowLight } =
+    useTrafficLight();
 
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
@@ -58,19 +15,13 @@ export const TrafficLightWithHook = () => {
         <div className="w-64 bg-gray-700 rounded-full h-2">
           <div
             className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
-            style={{ width: `${(countdown / 5) * 100}%` }}
+            style={{ width: `${percentage}%` }}
           ></div>
         </div>
 
-        <div
-          className={`w-32 h-32 ${light === "red" ? colors[light] : "bg-gray-500"} rounded-full`}
-        ></div>
-        <div
-          className={`w-32 h-32 ${light === "yellow" ? colors[light] : "bg-gray-500"} rounded-full`}
-        ></div>
-        <div
-          className={`w-32 h-32 ${light === "green" ? colors[light] : "bg-gray-500"} rounded-full`}
-        ></div>
+        <div className={`w-32 h-32 ${redLight} rounded-full`}></div>
+        <div className={`w-32 h-32 ${yellowLight} rounded-full`}></div>
+        <div className={`w-32 h-32 ${greenLight} rounded-full`}></div>
       </div>
     </div>
   );
